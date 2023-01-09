543cc57cf6e8f5feb172115c684958bd3dfa0b41
thomasvergne <thomas.vergne.contact@gmail.com>
2023-01-09 10:43:38 +0100

    ⚡️ Added let-expression parser.

---
+++ src/Language/Feather/Parser/Modules/LetExpressions.hs
@@ -0,0 +1,77 @@
+module Language.Feather.Parser.Modules.LetExpressions where
+  import Language.Feather.CST.Expression ( Expression(..), LetExpression(..) )
+  import Control.Applicative ( Alternative(some, (<|>)) )
+  import Language.Feather.CST.Literal ( Located((:>:)) )
+  import Language.Feather.Parser.Modules.Pattern ( patternExpression )
+  import Language.Feather.Parser.Modules.Operators ( operators )
+
+  import qualified Text.Parsec as P
+  import qualified Language.Feather.Parser.Lexer as L
+
+  letExpression :: Monad m => L.Feather m Expression -> L.Parser m LetExpression
+  letExpression expression =  P.try (letPatternExpression expression)
+                          <|> P.try (letInfixAbsExpression expression)
+                          <|> P.try (letInfixAbsClauseExpression expression)
+                          <|> P.try (letAbsExpression expression)
+                          <|> P.try (letAbsClauseExpression expression)
+                          <|> letExpression' expression
+
+  letExpression' :: Monad m => L.Feather m Expression -> L.Parser m LetExpression
+  letExpression' expr = LetExpression <$> (L.identifier <|> L.parens operators) <*> (L.reservedOp "=" *> expr)
+
+  letInfixAbsExpression :: Monad m => L.Feather m Expression -> L.Parser m LetExpression
+  letInfixAbsExpression expr = do
+    arg1 <- L.identifier <|> L.parens operators
+    name <- operators
+    arg2 <- L.identifier <|> L.parens operators
+    L.reservedOp "="
+    body <- expr
+    whereClause <- P.option [] (L.reserved "where" *> some (L.locate $ letExpression' expr))
+    return $ LetAbsExpression name [arg1, arg2] body whereClause
+
+  letInfixAbsClauseExpression :: Monad m => L.Feather m Expression -> L.Parser m LetExpression
+  letInfixAbsClauseExpression expr = do
+    arg1 <- L.identifier <|> L.parens operators
+    name <- operators
+    arg2 <- L.identifier <|> L.parens operators
+    clauses <- some (do
+      s <- P.getPosition
+      L.reservedOp "|"
+      e <- expr
+      L.reservedOp "="
+      e' <- expr
+      end <- P.getPosition
+      return $ (e, e') :>: (s, end))
+    whereClause <- P.option [] (L.reserved "where" *> some (L.locate $ letExpression' expr))
+    return $ LetAbsClauseExpression name [arg1, arg2] clauses whereClause
+  
+  letAbsExpression :: Monad m => L.Feather m Expression -> L.Parser m LetExpression
+  letAbsExpression expr = do
+    name <- L.identifier <|> L.parens operators
+    args <- some (L.identifier <|> L.parens operators)
+    L.reservedOp "="
+    body <- expr
+    whereClause <- P.option [] (L.reserved "where" *> some (L.locate $ letExpression' expr))
+    return $ LetAbsExpression name args body whereClause
+    
+  letAbsClauseExpression :: Monad m => L.Feather m Expression -> L.Parser m LetExpression
+  letAbsClauseExpression expr = do
+    name <- L.identifier <|> L.parens operators
+    args <- some (L.identifier <|> L.parens operators)
+    clauses <- some (do
+      s <- P.getPosition
+      L.reservedOp "|"
+      e <- expr
+      L.reservedOp "="
+      e' <- expr
+      end <- P.getPosition
+      return $ (e, e') :>: (s, end))
+    whereClause <- P.option [] (L.reserved "where" *> some (L.locate $ letExpression' expr))
+    return $ LetAbsClauseExpression name args clauses whereClause
+
+  letPatternExpression :: Monad m => L.Feather m Expression -> L.Parser m LetExpression
+  letPatternExpression expr = do
+    name <- patternExpression
+    L.reservedOp "="
+    body <- expr
+    return $ LetPatternExpression name body
