0eedfeaffe55598a16568c1c88d5fdd79e0602b7
thomasvergne <thomas.vergne.contact@gmail.com>
2023-01-09 10:51:12 +0100

    🏷️ Added type definition.

---
+++ src/Language/Feather/TypeChecker/Type.hs
@@ -0,0 +1,24 @@
+module Language.Feather.TypeChecker.Type where
+  data Type
+    = TVar Int
+    | Int | Float | Void | Bool | Char
+    | TId String
+    | TApp Type Type
+    deriving (Eq, Ord)
+  
+  instance Show Type where
+    show (TVar i) = "t" ++ show i
+    show Int = "int"
+    show Float = "float"
+    show Void = "void"
+    show Bool = "bool"
+    show Char = "char"
+    show (TId s) = s
+    show (TApp (TApp (TId "->") a) b) = "(" ++ show a ++ " -> " ++ show b ++ ")"
+    show (TApp (TApp (TId "(,)") a) b) = "(" ++ show a ++ ", " ++ show b ++ ")"
+    show (TApp (TId "[]") Char) = "string"
+    show (TApp (TId "[]") a) = "[" ++ show a ++ "]"
+    show (TApp a b) = "(" ++ show a ++ " " ++ show b ++ ")"
+
+  data Scheme = Forall [Int] Type
+    deriving (Eq, Ord, Show)
