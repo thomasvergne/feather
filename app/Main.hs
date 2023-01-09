5ee4c8480f02dd939dd968f1030426c5fd3c09de
thomasvergne <thomas.vergne.contact@gmail.com>
2023-01-09 10:52:03 +0100

    🔧 Added main entry script.

---
+++ app/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+  import Language.Feather.Parser.Parser
+  import Language.Feather.Pretty.AST ()
+  import Language.Feather.AST.Transform
+  import Language.Feather.TypeChecker.Checker
+
+  main :: IO ()
+  main = do
+    let name = "example/index.feather"
+    x <- readFile name
+    case parseFeather name x of
+      Left err -> print err
+      Right ast -> do
+        let ast' = removeNilExpressions $ transformExpressions ast
+        let ast'' = runChecker ast'
+        case ast'' of
+          Left err -> print err
+          Right ast' -> print ast'
